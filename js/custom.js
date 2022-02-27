3f3278f22063248dc6c6d794ac770a93a5545b20
MadiyarZh <mzhorakhan00@gmail.com>
2022-02-27 23:11:30 +0600

    style

--- js/custom.js
+++ js/custom.js
@@ -9,21 +9,21 @@ $( "input[type=radio][name=baz]:checked" ).val();
 
 // Accordion 
 
-$(".checkbox").click(function () {
-    $('.accordion .contnet').css('max-height', '1px');
-});
+// $(".checkbox").click(function () {
+//     $('.accordion .contnet').css('max-height', '1px');
+// });
 
-var acc = document.getElementsByClassName("checkbox");
-var i;
+// var acc = document.getElementsByClassName("checkbox");
+// var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//             panel.style.maxHeight = null;
+//         } else {
+//             panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }
