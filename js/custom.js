bab435bfba64445b1a86c19f21cd89a39319195d
MadiyarZh <mzhorakhan00@gmail.com>
2022-02-27 23:01:02 +0600

    layout

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+// Filter block
+
+$(".checked-year").bind("click", function() {
+    $('#selected-year').val(this.value);
+});
+
+$( "input[type=radio][name=baz]:checked" ).val();
+
+
+// Accordion 
+
+$(".checkbox").click(function () {
+    $('.accordion .contnet').css('max-height', '1px');
+});
+
+var acc = document.getElementsByClassName("checkbox");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
